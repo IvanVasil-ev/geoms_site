5d769326f880caf0c23b20c04ff7b6d745252e3c
Ivan Vasilev <i.vasilev@dunice.net>
2022-09-05 09:11:25 +0300

    Add resize block after section

--- general/js/main.js
+++ general/js/main.js
@@ -3,13 +3,19 @@ window.scrollTo(0, 0);
 window.addEventListener('load', () => {
     // <!-- Selectors  -->
     const DOCUMENT = document.getElementById('html');
+
     const SIDEBAR = document.getElementById('sidebar');
     const FLOAT_BUTTON = document.getElementById('float_button');
     const SIDEBAR_CLOSE = document.getElementById('sidebar-close');
+
     const MAIN_BLOCK = document.getElementById('main');
+
     const WELCOME_BLOCK = document.getElementById('welcome');
     const WELCOME_SQUARE = document.getElementById('welcome-square');
     const WELCOME_FLOAT = document.getElementById('welcome-float');
+
+    const ABOUT_BLOCK = document.getElementById('about');
+    const ABOUT_WHO = document.getElementById('about__who');
 
     // <!-- Onload  -->
     MAIN_BLOCK.classList.add('main_opacity_visible');
@@ -17,6 +23,7 @@ window.addEventListener('load', () => {
     // <!-- Constants -->
     const welcomeBlockRect = WELCOME_BLOCK.getBoundingClientRect();
     const welcomeFloatRect = WELCOME_FLOAT.getBoundingClientRect();
+    const aboutBlockRect = ABOUT_BLOCK.getBoundingClientRect();
 
     // <!-- Functions  -->
     const toggleSidebar = () => {
@@ -24,13 +31,16 @@ window.addEventListener('load', () => {
         SIDEBAR.classList.toggle('sidebar-hidden');
     };
 
-    const getValueByPercent = (config, { offset = 0, fromTop = true }) => {
+    const getValueByPercent = (config, { offset = 0, fromTop = true, endTop = true }) => {
+        // TODO: Необходимо переписать логику и добавить возможность отслеживания вхождения компонента в экран
         if (!config) {
             return;
         }
 
         const { y, height } = config;
-        const scrollValue = fromTop ? window.scrollY : window.scrollY + window.innerHeight;
+        const scrollTopValue = window.scrollY;
+        const scrollBotValue = window.scrollY + window.innerHeight;
+        const isBlockVisible = scrollTopValue < (y + height) || scrollBotValue < y;
         const isBlockActive = scrollValue > y && scrollValue < y + height;
         const percent = (height - offset) / 100;
         const coverage = (scrollValue - y) / percent;
@@ -44,14 +54,22 @@ window.addEventListener('load', () => {
 
     // <!-- Listeners  -->
     window.addEventListener('scroll', () => {
-        const [isWelcomeBlock, getWelcomeValue] = getValueByPercent(welcomeBlockRect, { offset: 200 });
+        const [isWelcomeBlock, getWelcomeValue] = getValueByPercent(welcomeBlockRect, {});
+        const [isAboutBlock, getAboutValue] = getValueByPercent(aboutBlockRect, { fromTop: false, endTop: false });
 
         if (isWelcomeBlock) {
             const newScale = getWelcomeValue({ min: 1, max: 2 });
             const newRotate = getWelcomeValue({ max: 90 });
             WELCOME_SQUARE.style.transform =  `scale(${newScale}, ${newScale}) rotate(-${newRotate}deg)`;
         } else {
-            WELCOME_SQUARE.style.transform = `scale(1, 1) rotate(0deg)`;
+            WELCOME_SQUARE.style.transform = 'scale(1, 1) rotate(0deg)';
+        }
+
+        if (isAboutBlock) {
+            const newScale = getAboutValue({ min: 1, max: 2 });
+            ABOUT_WHO.style.transform = `scale(${newScale}, ${newScale})`;
+        } else {
+            ABOUT_WHO.style.transform = 'scale(1, 1)';
         }
 
     }, { capture: true });
